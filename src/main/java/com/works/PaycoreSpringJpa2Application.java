76400490682dd9c863cca20464143f2ab290bef1
Hakan ÖZER <hakanozer02@gmail.com>
2022-10-24 18:47:28 +0300

    1-Days

---
+++ src/main/java/com/works/PaycoreSpringJpa2Application.java
@@ -0,0 +1,13 @@
+package com.works;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaycoreSpringJpa2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaycoreSpringJpa2Application.class, args);
+    }
+
+}
